93563dd6837d59d796503fc8ad9b9a83b8b5c46c
Lloyd Griffiths <lloyd.griffiths.sw@gmail.com>
2013-07-30 16:12:43 +0100

    Changed tabs to spaces

--- lib/Data/LinkedList/Entry.pm
+++ lib/Data/LinkedList/Entry.pm
@@ -4,18 +4,20 @@ use strict;
 use warnings;
 
 sub new {
-	my ($class, %params) = @_;
-	my ($self) = {
-		data     => undef,
-		next     => undef,
-		previous => undef
-	};
+    my ($class, %params) = @_;
+    my ($self) = {
+        data     => undef,
+        next     => undef,
+        previous => undef
+    };
 	
-	while (my ($key, $value) = each %params) {
-		$self->{$key} = $value if exists $self->{$key};
-	}
+    while (my ($key, $value) = each %params) {
+        $self->{$key} = $value if exists $self->{$key};
+    }
 	
-	return bless $self, $class;
+    return bless $self, $class;
 }
 
 1;
+
+__END__
